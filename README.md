cd3b9350b35fe78c007de915b7a4279ee3986935
lunhg <lunhg@users.noreply.github.com>
2018-02-01 11:08:33 -0200

    adicionados primeiros arquivos

---
+++ README.md
@@ -0,0 +1,65 @@
+![coverage](/coverage/badge.svg)
+
+# Pingado-Server
+
+_backend stack_ para desenvolvimento rápido de aplicações web.
+
+## Caracteristicas
+
+  - Assíncrono ([Bluebird](https://github.com/petkaantonov/bluebird))
+  - configuração de aplicação através variáveis de ambiente locais com dotenv;
+  - elaboração de REST api com express 4.*;
+  - Integração com base de dados ([Mongoose](http://mongoosejs.com/) por padrão);
+  - templates com [pug.js](https://github.com/pugjs)
+  - _Asset pipeline_ com [connect-assets](https://github.com/TrevorBurnham/connect-assets);
+  - registro de atividade (log) e exceções;
+
+## Instalar
+
+    git clone https://www.github.com/lunhg/pingado.git
+	npm install
+
+## Configuração
+
+Crie um arquivo `.env` na pasta raiz do seu projeto como o a seguir. 
+
+
+    CAFE_RAILS_PORT=3000
+    CAFE_RAILS_LOGGER=':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent'
+    CAFE_RAILS_VIEWS='/minha/pasta/app/views'
+    CAFE_RAILS_ENGINE='pug'
+    CAFE_RAILS_PUBLIC='/minha/pasta/app/assets'
+    CAFE_RAILS_IMAGE='/minha/pasta/app/assets/images'
+    CAFE_RAILS_FONTS='/minha/pasta/app/assets/fonts'
+    CAFE_RAILS_STYLES='/minha/pasta/app/assets/css'
+    CAFE_RAILS_SCRIPTS='/minha/pasta/app/assets/js'
+    CAFE_RAILS_COVERAGE='/minha/pasta/coverage/lcov-report'
+    CAFE_RAILS_DOCUMENTATION='/minha/pasta/app/assets/doc'
+	BLUEBIRD_LONG_STACK_TRACES=1 
+	BLUEBIRD_WARNINGS=1
+
+Variáveis:
+
+  - `CAFE_RAILS_PORT`: porta de execução do servidor
+  - `CAFE_RAILS_LOGGER`: formato de registro dos acessos
+  - `CAFE_RAILS_VIEWS`: pasta onde estão localizados os templates html 
+  - `CAFE_RAILS_ENGINE`: motor utilizado para converter os templates html
+  - `CAFE_RAILS_PUBLIC`: pasta onde estão localizados os arquivos públicos 
+  - `CAFE_RAILS_IMAGE`: pasta onde estão localizados os arquivos de imagem
+  - `CAFE_RAILS_FONTS`: pasta onde estão localizados os arquivos de imagem
+  - `CAFE_RAILS_STYLES`: pasta onde estão localizados os arquivos de estilos
+  - `CAFE_RAILS_SCRIPTS`: pasta onde estão localizados os arquivos de javascript
+  - `CAFE_RAILS_SCRIPTS`: pasta onde estão localizados os code-coverages
+  - `CAFE_RAILS_SCRIPTS`: pasta onde estão localizados os arquivos de documentação
+  - `BLUEBIRD_LONG_STACK`:habilita reportação de erros nas promessas 
+  - `BLUEBIRD_WARNINGS`: idem
+  
+
+## Testar
+
+	npm test
+
+Isso executará um servidor teste em http://localhost:3000 que irá testar:
+
+- GET /
+- POST /
---
+++ test/server.js
@@ -0,0 +1,148 @@
+const should = require('should');
+const path = require('path');
+const Pingado = require('../index')
+const foreach = require('foreach');
+const fs = require('fs');
+let mongoose = require('mongoose');
+
+let env = path.join(__dirname, '.env')
+let envs = 'PORT DATABASE LOGGER VIEWS ENGINE PUBLIC IMAGES STYLES SCRIPTS COVERAGE DOCUMENTATION'
+
+Pingado.init(env).then(function(){
+    describe('Testing pingado', function(){
+
+	let suite = {};
+
+	it('should .env file exist', function(done){
+	    fs.readFile(env, 'utf8', function(err, data){
+		if(!err) done()
+		if(err) done(err)
+	    });
+	})
+
+	it('should export .env environment', function(done){    
+	    foreach(envs.split(' '), function(e,i,o){
+		process.env.should.have.property('PINGADO_'+e)
+	    })
+	    done()
+	});
+
+	it('should start mongodb', function(){    
+	    pingado = Pingado.database('mongodb').then(function(conn){
+		suite.connection = conn;
+		conn._readyState.should.equal(1)
+	    })
+	    return pingado;
+	});
+
+	it('should initialize logger', function(){    
+	    pingado = pingado.then(Pingado.logger).then(function(logger){
+		logger.warn('Log Test')
+		suite.logger = logger;
+		return logger
+	    })
+	    return pingado;
+	});
+
+	it('should configure app', function(){    
+	    pingado = pingado.then(Pingado.app).then(function(app){
+		mongoose.model('Cookie', mongoose.Schema({
+		    value: String,
+		    path: String
+		}))
+		mongoose.model('Token', mongoose.Schema({
+		    value: String
+		}))
+		mongoose.model('Test', mongoose.Schema({
+		    cookie: {type: mongoose.Schema.Types.ObjectId, ref: 'Cookie'},
+		    token: {type: mongoose.Schema.Types.ObjectId, ref: 'Token'}
+		}))
+		let Test = mongoose.model('Test');
+		let Cookie = mongoose.model('Cookie');
+		let Token = mongoose.model('Token');
+		app.get('/', function(req, res){
+		    res.json({
+			_csrf:req.csrfToken()
+		    });
+		})
+		app.post('/', function(req, res){
+		    let c = req.headers['cookie'].split(";")
+		    let cookie = new Cookie({
+			value:c[0].split(";")[0].split("_csrf=")[1],
+			path:c[1].split("=")[1]
+		    })
+		    let token = new Token({
+			value:req.body._csrf
+		    })
+		    cookie.save()
+		    token.save()
+		    let test = new Test({cookie:cookie, token:token})
+		    test.save(function(err, t){
+			if(err) res.json({message:err.message,code:err.code,stack:err.stack.split("\n")})
+			res.json(t)
+		    });
+		})
+		suite.app = app;
+		return app
+	    })
+	    return pingado;
+	});
+
+	it('should serve', function(){    
+	    pingado = pingado.then(Pingado.serve).then(function(server){
+		suite.server = server
+		let addr = server.address();
+		suite.logger.warn('Running '+addr.family+' server on '+addr.address+addr.port)
+		suite.agent = require('supertest').agent('http://localhost:3000');
+		return server
+	    })
+	    return pingado;
+	});
+
+	it('should GET /', function(done){    
+	    suite.agent.get('/')
+		.expect(200)
+		.expect('Content-Type', /json/)
+		.expect(function(res){
+		    res.body.should.have.property('_csrf')
+		    res.body._csrf.should.match(/[a-zA-Z0-9\-\_]+/);
+		    suite.token = res.body._csrf
+		    suite.cookie = res.headers['set-cookie']
+		    
+		}).end(done)
+	});
+
+	it('should POST /', function(){    
+	    return new Promise(function(resolve, reject){
+		suite.agent.post('/')
+		    .set('cookie', suite.cookie)
+		    .send({_csrf:suite.token})
+		    .expect(200)
+		    .expect('Content-Type', /json/)
+		    .expect(function(res){
+			console.log(res.text);
+			res.body.should.have.property('_id');
+			res.body.should.have.property('cookie');
+			res.body.should.have.property('token');
+			res.body._id.should.match(/[a-z0-9]+/)
+			res.body.cookie._id.should.match(/[a-z0-9]+/)
+			res.body.token._id.should.match(/[a-z0-9]+/)
+			res.body.cookie.value.should.match(/[a-z0-9A-Z\-\_]+/)
+			res.body.token.value.should.match(/[a-z0-9A-Z\-\_]+/)
+			res.body.token.value.length.should.be.above(res.body.cookie.value.length)
+		    }).then(resolve).catch(reject)
+		})
+	    });
+
+	after(function(){
+	    suite.server.close(function(){
+		suite.logger.warn('End of test')
+		suite.connection.close()
+	    })
+	});
+	
+	run();
+    })
+    
+})
+
